6892b8d5ef6c0c527bb2c33e626eef5aea92c806
Or Ricon <or.ricon@dfinity.org>
2024-02-13 15:29:29 +0000

    ok

--- my-server/src/main.rs
+++ my-server/src/main.rs
@@ -1,12 +1,23 @@
-#[macro_use]
-extern crate rocket;
+use anyhow::{Context, Error};
+use axum::{routing::get, Router};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("failed to listen to address")?;
+
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app)
+        .await
+        .context("failed to serve")?;
+
+    Ok(())
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn handler() -> &'static str {
+    "Hello"
 }
